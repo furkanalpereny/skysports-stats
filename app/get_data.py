b592423a099f350bd3248354cfc9b4b7ba4ed617
furkanalpereny <furkan.aalperen@gmail.com>
2020-01-12 03:40:53 +0300

    project has finished

--- app/get_data.py
+++ app/get_data.py
@@ -3,14 +3,17 @@ import requests
 import json
 import pprint
 import os
+import pandas
 from datetime import datetime
 
 mainLink = "https://www.skysports.com"
+
 
 def soup(link):
     r = requests.get(link)
     soup = BeautifulSoup(r.content, "html.parser")
     return soup
+
 
 def get_leagues():
     leagues = []
@@ -20,52 +23,58 @@ def get_leagues():
     for li in ul:
         li = li.find_all('li')
         for li_item in li:
-            a = li_item.find_all('a',href=True)
+            a = li_item.find_all('a', href=True)
             leagues.append(a[0]['href'])
     return leagues
 
+
 def get_seasons(league):
-    link = mainLink + league +"-results"
-    gelenVeri = soup(link) 
+    link = mainLink + league + "-results"
+    gelenVeri = soup(link)
     gelenVeri = gelenVeri.find_all('div', attrs={'class': 'section-nav__body'})
-    gelenVeri = gelenVeri[0].find_all('ul', attrs={'class': 'section-nav__group'})
-    gelenVeri = gelenVeri[0].find_all('li', attrs={'class': 'section-nav__item'})
+    gelenVeri = gelenVeri[0].find_all(
+        'ul', attrs={'class': 'section-nav__group'})
+    gelenVeri = gelenVeri[0].find_all(
+        'li', attrs={'class': 'section-nav__item'})
 
     seasons = []
     for i in gelenVeri:
-        satir = i.find_all('a',href=True)
+        satir = i.find_all('a', href=True)
         seasons.append(satir[0]['href'])
 
     return seasons
 
+
 def push_stats(link):
     a = link.split('/')
-    a.insert(5,"stats")
+    a.insert(5, "stats")
     new_link = ""
     for i in a:
         new_link = new_link + i + '/'
     return new_link
 
+
 def get_matches(season):
     link = mainLink + season
     matches = []
-    gelenVeri = soup(link) 
+    gelenVeri = soup(link)
     gelenVeri = gelenVeri.find_all('div', attrs={'class': 'fixres__body'})
 
-    #ilk 200
+    # ilk 200
     ilk200 = gelenVeri[0].find_all('div', attrs={'class': 'fixres__item'})
     for i in ilk200:
         a = i.find_all('a', href=True)
         href = a[0]['href']
         href = push_stats(href)
         matches.append(href)
-    
-    #son 180
+
+    # son 180
     try:
-        script = gelenVeri[0].find_all('script', attrs={'type': 'text/show-more'})[0].text
+        script = gelenVeri[0].find_all(
+            'script', attrs={'type': 'text/show-more'})[0].text
         script = BeautifulSoup(script, "html.parser")
         script = script.find_all('div', attrs={'class': 'fixres__item'})
-        
+
         for i in script:
             a = i.find_all('a', href=True)
             href = a[0]['href']
@@ -77,22 +86,27 @@ def get_matches(season):
 
     return matches
 
+
 def make_column_name(name):
     name = name.rstrip()
     name = name.lower()
-    name = name.replace(' ','_')
+    name = name.replace(' ', '_')
     return name
+
 
 def get_stats(link):
     _dict = {}
     rawData = soup(link)
 
-    #MATCH DETAILS
-    match_details = rawData.find_all("div",attrs={"class":"match-head__fixture-side match-head__fixture-side--wide-score"})
+    # MATCH DETAILS
+    match_details = rawData.find_all("div", attrs={
+                                     "class": "match-head__fixture-side match-head__fixture-side--wide-score"})
 
     for idx, item in enumerate(match_details):
-        team_name = item.find_all("abbr",attrs={"class":"swap-text--bp10"})[0]["title"]
-        team_score = item.find_all("span",attrs={"class":"match-head__score"})[0].text
+        team_name = item.find_all(
+            "abbr", attrs={"class": "swap-text--bp10"})[0]["title"]
+        team_score = item.find_all(
+            "span", attrs={"class": "match-head__score"})[0].text
         team_score = team_score.strip()
         if idx == 0:
             _dict["team_name_home"] = team_name
@@ -101,23 +115,27 @@ def get_stats(link):
             _dict["team_name_away"] = team_name
             _dict["team_score_away"] = team_score
 
-    #STATISTICS
-    match_stats = rawData.find_all("div",attrs={"class":"match-stats callfn"})
-    match_stats_item = match_stats[0].find_all("div",attrs={"class":"match-stats__item"})
+    # STATISTICS
+    match_stats = rawData.find_all(
+        "div", attrs={"class": "match-stats callfn"})
+    match_stats_item = match_stats[0].find_all(
+        "div", attrs={"class": "match-stats__item"})
     for idx, item in enumerate(match_stats_item):
-        column_name = item.find_all("h5",attrs={"class":"match-stats__name"})
+        column_name = item.find_all("h5", attrs={"class": "match-stats__name"})
         column_name = column_name[0].text
-        column_name = make_column_name(column_name)  
+        column_name = make_column_name(column_name)
 
-        stat = item.find_all("span",attrs={"class":"match-stats__bar", "data-role":"match-stat-home"})[0].text
+        stat = item.find_all("span", attrs={
+                             "class": "match-stats__bar", "data-role": "match-stat-home"})[0].text
 
-        _dict[str(column_name + "_home")] = stat 
+        _dict[str(column_name + "_home")] = stat
 
-        stat = item.find_all("span",attrs={"class":"match-stats__bar", "data-role":"match-stat-away"})[0].text
+        stat = item.find_all("span", attrs={
+                             "class": "match-stats__bar", "data-role": "match-stat-away"})[0].text
 
-        _dict[str(column_name + "_away")] = stat 
+        _dict[str(column_name + "_away")] = stat
 
-    #MATCH RESULT
+    # MATCH RESULT
     if _dict["team_score_home"] > _dict["team_score_away"]:
         _dict["match_result"] = "1"
     elif _dict["team_score_home"] < _dict["team_score_away"]:
@@ -125,26 +143,26 @@ def get_stats(link):
     else:
         _dict["match_result"] = "0"
 
-
     pprint.sorted = lambda x, key=None: x
 
     return _dict
-    
+
+
 def write_json(season, league):
     print("Getting matches...")
     matches = get_matches(season)
     stats = []
 
-    seasonPath = season.replace('/','_')
-    filePath = "json/" + league.replace('/','') + '/'
+    seasonPath = season.replace('/', '_')
+    filePath = "json/" + league.replace('/', '') + '/'
     json_name = filePath + seasonPath + '.json'
 
-    if not os.path.exists(filePath): 
+    if not os.path.exists(filePath):
         os.makedirs(filePath)
-        
+
     if not os.path.exists(json_name):
         for idx, item in enumerate(matches):
-            print("{0}/{1} => {2}".format(idx,len(matches),item))
+            print("{0}/{1} => {2}".format(idx, len(matches), item))
             try:
                 stats.append(get_stats(item))
             except:
@@ -153,14 +171,14 @@ def write_json(season, league):
                 if os.path.exists("logs.json"):
                     with open("logs.json", "r") as jsonFile:
                         data = json.load(jsonFile)
-                
+
                 try:
                     err_id = int(data[-1]["id"]) + 1
                 except:
                     err_id = 0
 
                 log = {
-                    "id": err_id, 
+                    "id": err_id,
                     "type": "error",
                     "time": str(datetime.now()),
                     "league": league,
@@ -174,30 +192,63 @@ def write_json(season, league):
 
         print("Creating {0}".format(seasonPath))
         with open(json_name, 'w') as outfile:
-                json.dump(stats, outfile, ensure_ascii=False, indent=4)
+            json.dump(stats, outfile, ensure_ascii=False, indent=4)
     else:
         print("Already exist. Missing " + json_name)
-    
+
+
+def get_paths():
+    folder = os.listdir("json")
+    all_folders = []
+    for i in folder:
+        json_file = "json/" + i
+        files = os.listdir(json_file)
+
+        main_folder = {
+            "main_folder": i,
+            "edge_files": files
+        }
+
+        all_folders.append(main_folder)
+
+    return all_folders
+
+
+def json_to_xlsx(path):
+    df = pandas.read_json(path)
+    path = path.replace("json", "xlsx")
+    df.to_excel(path, index=False)
+    return path
+
+
+def json_to_csv(path):
+    df = pandas.read_json(path)
+    path = path.replace("json", "csv")
+    df.to_csv(path, index=False)
+    return path
+
+
 def main():
     print("Welcome to SkySports Puller v0.0.1")
     print("Loading leagues, please wait...\n")
     leagues = get_leagues()
     for idx, item in enumerate(leagues):
-        print("\t{0}. {1}".format(idx,item))
+        print("\t{0}. {1}".format(idx, item))
 
     index = -1
     while 0 > index or len(leagues)-1 < index:
         try:
             index = int(input("\nEnter index of league: "))
         except ValueError:
-            print ("Please enter a numeric value!")
+            print("Please enter a numeric value!")
 
     league = leagues[int(index)]
     seasons = get_seasons(league)
 
-    for i in range(0,9):
+    for i in range(0, 9):
         print("Getting matches of " + seasons[i])
         write_json(seasons[i], league)
-    
+
+
 if __name__ == '__main__':
     main()
---
+++ app/routes.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, jsonify, render_template, current_app, send_from_directory, send_file
+from app import app, get_data
+import os
+import time
+import json
+
+
+@app.route('/')
+def main():
+    paths = get_data.get_paths()
+    return render_template("index.html", paths=paths)
+
+
+@app.route('/json/<path:filename>', methods=['GET', 'POST'])
+def download_json(filename):
+    uploads = os.path.join(current_app.root_path, "data")
+    return send_from_directory(directory=uploads, filename=filename)
+
+
+@app.route('/xlsx/<path:filename>', methods=['GET', 'POST'])
+def download_xlsx(filename):
+    uploads = os.path.join(current_app.root_path, "data")
+    path = uploads+"/"+filename
+    path = get_data.json_to_xlsx(path)
+    return send_file(path, as_attachment=True)
+
+
+@app.route('/csv/<path:filename>', methods=['GET', 'POST'])
+def download_csv(filename):
+    uploads = os.path.join(current_app.root_path, "data")
+    path = uploads+"/"+filename
+    path = get_data.json_to_csv(path)
+    return send_file(path, as_attachment=True)
